124a046c6eed4f69f74c2943900424e1d0bd8a1e
Jordy Vieira <jordyvieira@hotmail.com>
2021-09-06 12:01:15 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+          <div className="container">
+            <p className="text-light">App desenvolvido por <a href="https://github.com/vieiraa" target="_blank" rel="noreferrer">Jordy Vieira</a></p>
+          </div>
+        </footer>
+    )
+}
+
+export default Footer
