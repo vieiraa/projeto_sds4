7e04edc60523120d59061fbd6fe16b3ab818a806
Jordy Vieira <jordyvieira@hotmail.com>
2021-09-10 14:55:00 -0300

    DataTable integration

--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -1,4 +1,11 @@
+import { format } from 'date-fns'
 export function round(value: number, precision: number) {
     const multiplier = Math.pow(10, precision || 0)
     return Math.round(value * multiplier) / multiplier
+}
+
+export function formatLocalDate(date: string, pattern: string) {
+    const d = new Date(date)
+    const dateOnly = new Date(d.valueOf() + d.getTimezoneOffset() * 60 * 1000)
+    return format(dateOnly, pattern)
 }
